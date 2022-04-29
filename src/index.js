6d9cbf2f7cf39d9a8b3ac8fdf73f9fe34655ee4f
Jorge Repossi <jorgerepossi1980@gmail.com>
2022-04-28 23:57:42 -0300

    index.js changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-const Server = require("./models/server");
-/* 
-const { Server: HttpServer } = require( 'http' )
-const { Server: IOServer } = require( 'socket.io' )
-const productsRouter = require( './routes/product.js' )
-
-
-const httpServer = new HttpServer( app )
-const io = new IOServer( httpServer )
-
-app.set( 'views', 'src/views' )
-
-app.use( express.urlencoded( { extended: true } ) )
-app.use( express.json() ) */
-
+const Server = require( "./models/server" );
 const server = new Server();
 
 server.listen();
